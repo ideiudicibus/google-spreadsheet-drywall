d7e4c7a82e96c761d41829db17d1ec15c0d86db3
i.deiudicibus@gmail.com <i.deiudicibus@gmail.com>
2014-07-24 14:48:21 +0200

    fixed save simulation, start printable templates

---
+++ public/views/spreadsheets/dashboard/index-printable.js
@@ -0,0 +1,41 @@
+google.load("visualization", '1.1', {packages:['corechart','table']});
+
+
+var data=initData();
+
+function resetHtmlTemplates(){
+
+if( templateLoader.localStorageAvailable()){
+
+                localStorage.removeItem("templates");
+                localStorage.removeItem("templateVersion");
+               
+      }
+
+}
+
+function loadTemplates(spreadsheet){
+
+     var activeSheet=spreadsheet.sheetsList[0];
+     var tmplFile=spreadsheet.sheetsList[0]._id;
+           tmplFile=tmplFile.split('-')[1];
+           tmplFile="2-"+tmplFile;
+       
+          templateLoader.loadRemoteTemplate(activeSheet._id, "/views/spreadsheets/dashboard/"+tmplFile+"-printable-tmpl.html?", 
+            function(data) {
+              var compiled = _.template(data);
+              $('#printable').html(compiled({textNote:activeSheet.textNote,activeSheetName:activeSheet.name,params:JSON.parse(activeSheet.params)}));
+            });
+
+}
+
+$(function() {
+
+resetHtmlTemplates();
+
+loadTemplates(data.record);
+
+
+
+
+});
